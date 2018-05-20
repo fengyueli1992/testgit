c34e5415f875417b7086635f6f96277c3f4266c0
fyl <99168926@qq.com>
2018-05-21 00:11:01 +0800

    1

--- src/com/youku/atm/TestGit2.java
+++ src/com/youku/atm/TestGit2.java
@@ -4,11 +4,7 @@ public class TestGit2 {
   public static void main(String []args){
     System.out.println("haha");
 
-    System.out.println("ha1ha");
-     System.out.println("haha111");
-    
-
-     System.out.println("haha111");
+  
 
   }
 
